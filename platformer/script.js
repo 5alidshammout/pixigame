1f0357c3b66107c422fb42310808dd89512ad532
Khalid shammout <5alidshammout@gmail.com>
2021-12-22 17:56:03 +0200

    TilingSprite

--- platformer/script.js
+++ platformer/script.js
@@ -1,4 +1,4 @@
-let app = new PIXI.Application({ width: 960, height: 600 });
+let app = new PIXI.Application({ width: 1000, height: 600 });
 let folder = 'Sunnyland/PNG/';
 PIXI.Loader.shared
 	.add(folder + 'environment/layers/back.png')
@@ -6,18 +6,23 @@ PIXI.Loader.shared
 	.load(setup);
 
 function setup() {
-	let back = new PIXI.Sprite(
-		PIXI.Loader.shared.resources[folder + 'environment/layers/back.png'].texture
+	let back = new PIXI.TilingSprite(
+		PIXI.Loader.shared.resources[
+			folder + 'environment/layers/back.png'
+		].texture,
+		app.screen.width,
+		app.screen.height
 	);
-	back.height = app.screen.height;
-	back.width = app.screen.height * 1.6;
-
-	let middle = new PIXI.Sprite(
+	back.scale.set(app.screen.height / 240);
+	let middle = new PIXI.TilingSprite(
 		PIXI.Loader.shared.resources[
 			folder + 'environment/layers/middle.png'
-		].texture
+		].texture,
+		app.screen.width + 50,
+		368
 	);
 	middle.position.y = app.screen.height - middle.height;
+	middle.position.x -= 50;
 
 	app.stage.addChild(back, middle);
 }

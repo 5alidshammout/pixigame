6adadec2fb3f7a8a2b74561bf1c6c22ddadb632a
Khalid shammout <5alidshammout@gmail.com>
2022-01-07 07:13:30 +0200

    try to colllide

--- platformer/script.js
+++ platformer/script.js
@@ -10,6 +10,11 @@ PIXI.Loader.shared
 
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
+let player;
+let gravity = 10;
+let size = 32;
+let collidingTiles = ['000', '001', '002', '075', '076', '077'];
+
 function setup() {
 	let back = new PIXI.TilingSprite(
 		PIXI.Loader.shared.resources[
@@ -34,8 +39,6 @@ function setup() {
 
 	let textures = PIXI.Loader.shared.resources['json'].textures;
 
-	let size = 32;
-
 	let maps = PIXI.Loader.shared.resources['maps'].data.maps;
 	let arr = maps.LVL1.map;
 
@@ -52,19 +55,46 @@ function setup() {
 
 	let playersheet = PIXI.Loader.shared.resources['player'];
 
-	let player = new PIXI.AnimatedSprite(
+	player = new PIXI.AnimatedSprite(
 		playersheet.spritesheet.animations['player-idle']
 	);
 
 	player.animationSpeed = 0.1;
 	player.anchor.set(0, 1);
-	player.position.set(0, app.screen.height - 32 * 4);
+	player.position.set(32, 32 * 17);
 	player.scale.set((2 * size) / player.width);
 	player.play();
 
-	console.log(playersheet.spritesheet.animations['player-idle']);
+	// PIXI.Ticker.shared.add(_ => {
+	// 	player.position.y += gravity;
+	// });
 
 	app.stage.addChild(back, middle, tilemap, player);
 }
 
 document.body.appendChild(app.view);
+
+function getTileData(x, y) {
+	let arr = PIXI.Loader.shared.resources['maps'].data.maps.LVL1.map;
+	return [
+		arr[y][x],
+		collidingTiles.includes(arr[y][x]),
+		{
+			x: x * size,
+			y: y * size,
+			width: size,
+			height: size,
+		},
+	];
+}
+function tileCollide(sprite, tile) {
+	let abox = sprite.getBounds();
+	let bbox = tile[2];
+
+	return (
+		abox.x + abox.width > bbox.x &&
+		abox.x < bbox.x + bbox.width &&
+		abox.y + abox.height > bbox.y &&
+		abox.y < bbox.y + bbox.height
+	);
+}
